083f039398be204ab7f25b4162d4eca9b48f269e
Dag Einar Monsen <me@dag.im>
2013-05-06 15:30:04 +1000

    Add events. Move logging to Crawler.js and add verbose mode.

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ This crawler is given as an assigment in the RMIT course [COSC1165 Intelligent W
 * Polite: Supports robots.txt and meta/rel markup. Won't flood servers.
 * Robust: Handles multiple crawler traps.
 * Selective: Limit to a certain host or subpath on that host.
+
+### Installation
+
+Note: Will deploy to npm when more mature.
+
+
+### Usage 
+
+**fireleg [options] <seed-url>**
+
+  Options:
+
+    -h, --help                  output usage information
+    -V, --version               output the version number
+    -p, --politeness <seconds>  The politeness of the crawler
+    -m, --maxpages <pages>      The maximum page limit for the crawler
+    -m, --limithost <boolean>   Whether to limit crawling to the same hostname
--- crawl.js
+++ crawl.js
@@ -1,33 +1,29 @@
 #!/usr/bin/env node --debug
 var program = require('commander');
-var _ = require('underscore');
-var requirejs = require('requirejs');
+var _ = require('underscore'),
+  moment = require('moment');
 
-requirejs.config({
-  baseUrl:  'lib',
-  nodeRequire: require
-});
+var info = require('./package.json');
 
 program
-  .version('0.0.1')
+  .version(info.version)
   .usage('[options] <seed-url>')
-  .option('-p, --politeness <seconds>', 'The politeness of the crawler', parseInt)
-  .option('-m, --maxpages <pages>', 'The maximum page limit for the crawler', parseInt)
-  .option('-m, --limithost <boolean>', 'Whether to limit crawling to the same hostname', Boolean)
+  .option('-v, --verbose', 'Whether to print output or not')
+  .option('-p, --politeness <seconds>', 'The politeness of the crawler', parseInt, void(0))
+  .option('-m, --maxpages <pages>', 'The maximum page limit for the crawler', parseInt, void(0))
+  .option('-m, --limithost', 'Whether to limit crawling to the same hostname')
+  .option('-m, --limitpath', 'Whether to limit crawling to the path')
   .parse(process.argv);
 
-if(!program.args.length > 0) {
+if (!program.args.length > 0) {
   program.help();
 }
 
-requirejs(['Utility'], function(Utility) {
-  Utility(global);
+var params = _.pick(program, 'politeness', 'maxpages', 'limithost', 'limitpath');
+var seedURL = _.first(program.args);
 
-  requirejs(['Crawler'], function (Crawler) {
+var crawler = new (require('./lib/Crawler'))(params);
+var fileHandler = new (require('./lib/FileHandler'))(crawler.options.dataDir);
 
-    var params = _.pick(program, 'politeness', 'maxpages', 'limithost');
-    var seed = _.first(program.args);
 
-    new Crawler(params).crawl(seed);
-  })
-});
+crawler.crawl(seedURL);
